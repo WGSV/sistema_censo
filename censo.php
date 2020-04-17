164d813ead256db323efe19959e09fcdf0d59ee6
Mario Cabrera <MaritoCx@users.noreply.github.com>
2020-04-17 14:28:27 -0600

    cambio al formulario

--- censo.php
+++ censo.php
@@ -1,8 +1,51 @@
+<div>
+  <h1>CENSO NACIONAL DE EL SALVADOR</h1>
+  <HR></HR>
+</div>
 <form>
   <div class="form-group">
-    <label for="exampleFormControlInput1">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-  </div>
+    <h4>Anfitrión del hogar</h4>
+    <label for="nombres_formulario">Nombres</label>
+    <input type="name" class="form-control" id="nombres_formulario" placeholder="Primer y segundo nombre">
+    <label for="apellidos_formulario">Apellidos</label>
+    <input type="name" class="form-control" id="apellidos_formulario" placeholder="Primer y segundo apellido">
+    <div class="row">
+      <div class="col-lg-6 col-md-6">
+          <label for="dui_formulario">DUI</label>
+          <input type="name" class="form-control" id="dui_formulario" placeholder="00000000-0">
+      </div>
+      <div class="col-lg-6 col-md-6">
+          <label for="nit_formulario">NIT</label>
+          <input type="name" class="form-control" id="nit_formulario" placeholder="0000-000-0000-00">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-6 col-md-6">
+          <label for="genero_formulario">Género</label>
+          <select class="form-control" id="genero_formulario">
+            <option>Masculino</option>
+            <option>Femenino</option>
+      </div>
+      <div class="col-lg-6 col-md-6">
+          <label for="contacto_formulario">Número de contacto</label>
+          <input type="name" class="form-control" id="contacto_formulario" placeholder="0000-0000">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-6 col-md-6">
+        <label for="departamento_forumulario">Departamento</label>
+        <select class="form-control" id="departamento_formulario">
+          <option>Santa Ana</option>
+          <option>Sonsonate</option>
+          <option>Ahuachapán</option>
+          <option>Sansalvador</option>
+      </div>
+      <div class="col-lg-6 col-md-6">
+        <label for="municipio_forumulario">Municipio</label>
+        <input type="name" class="form-control" id="municipio_formulario" placeholder="Ciudad">
+      </div>
+    </div>
+
   <div class="form-group">
     <label for="exampleFormControlSelect1">Example select</label>
     <select class="form-control" id="exampleFormControlSelect1">
