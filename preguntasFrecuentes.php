4b07ff94f116b91c44a78d1e6f802b2d6e8c9378
vjro14 <vjro_bmw@hotmail.com>
2020-04-17 14:01:41 -0600

    plantilla base para estadisticas y FAQ

---
+++ preguntasFrecuentes.php
@@ -0,0 +1,49 @@
+<h1 style="text-align: center;">Preguntas Frecuentes</h1>
+
+<div class="row" style="margin: 10%">
+    <div class="accordion" id="accordionFAQ">
+        <div class="card">
+            <div class="card-header" id="headingOne">
+                <h2 class="mb-0">
+                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#QuestionOne" aria-expanded="true" aria-controls="QuestionOne">
+                        Pregunta #1
+                    </button>
+                </h2>
+            </div>
+
+            <div id="QuestionOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionFAQ">
+                <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="headingTwo">
+                <h2 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#QuestionTwo" aria-expanded="false" aria-controls="QuestionTwo">
+                        Pregunta #2
+                    </button>
+                </h2>
+            </div>
+            <div id="QuestionTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionFAQ">
+                <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="headingThree">
+                <h2 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#QuestionThree" aria-expanded="false" aria-controls="QuestionThree">
+                        Pregunta #3
+                    </button>
+                </h2>
+            </div>
+            <div id="QuestionThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionFAQ">
+                <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
