8eb878cbf32790b348952425c156d8c63c059e94
vjro14 <vjro_bmw@hotmail.com>
2020-04-17 16:26:14 -0600

    pagina principal informacion

---
+++ informacion.php
@@ -0,0 +1,27 @@
+<h1 style="text-align: center">Informacion</h1>
+
+<div class="row" style="margin: 2%">
+    <div class="col-8">
+        <p style="text-align: justify">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac purus porttitor, aliquam ex ac, ornare orci. Donec nec pellentesque nulla. Mauris sagittis elit viverra, tincidunt elit vitae, mollis eros. Maecenas eu lacus viverra, dictum eros vitae, eleifend arcu. Proin nec augue risus. Nunc eget metus ex. Nunc vitae elementum neque, sed mattis felis. Integer rhoncus aliquam ante.
+        </p>
+        <p style="text-align: justify">
+            Ut scelerisque mauris et ipsum mollis maximus. Sed ut nunc imperdiet, posuere sapien vel, feugiat purus. Cras elementum faucibus nunc, eu tincidunt felis viverra in. Vivamus et diam elit. Vestibulum velit elit, varius sed gravida tristique, dictum a eros. Integer bibendum ante non pretium ultrices. Mauris mauris sem, mattis sed nibh vitae, aliquam facilisis odio. Maecenas nec iaculis neque.
+        </p>
+        <p style="text-align: justify">
+            Nulla bibendum convallis purus a hendrerit. Vestibulum quis tincidunt sapien. Suspendisse sem neque, condimentum vitae suscipit vel, ultrices at purus. Integer consectetur sapien ut justo hendrerit, eget consequat nibh placerat. Sed volutpat in erat vitae ullamcorper. Phasellus fermentum sodales ligula, in accumsan quam pharetra in. Vestibulum tincidunt blandit justo, non ornare elit fringilla sollicitudin. Nam luctus, tellus id maximus suscipit, nisl libero semper dolor, et tempor purus dui varius velit. Quisque varius lorem justo, et rutrum elit maximus sit amet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+        </p>
+        <p style="text-align: justify">
+            Praesent nec nulla libero. Pellentesque tristique pharetra nisi vitae commodo. Phasellus et venenatis tortor. Phasellus ultrices et lacus ac porta. Nulla vitae leo augue. Nulla scelerisque nisl pretium, mollis enim id, vestibulum turpis. Pellentesque hendrerit molestie cursus. Quisque pretium vehicula elit, vitae suscipit elit aliquet ut. Cras auctor, enim quis luctus luctus, nulla purus vehicula erat, porttitor tempus sapien enim sit amet eros. Cras quis mollis massa. In eu facilisis libero. Mauris sit amet sem viverra, porta leo ac, rutrum augue. Proin eu dictum mi, eu laoreet urna.
+        </p>
+        <p style="text-align: justify">
+            Integer pellentesque lacus ac felis cursus, in cursus risus fringilla. Aenean elementum nisi tincidunt lorem iaculis malesuada. Nulla lacinia erat eget facilisis blandit. Nullam semper turpis quis fringilla venenatis. Morbi tristique justo dapibus tortor euismod tristique. In hac habitasse platea dictumst. Quisque fringilla nunc leo, vitae finibus magna pellentesque ut. Nam viverra mattis turpis nec lacinia. Sed purus metus, laoreet vitae ante in, mollis dictum neque. Aenean quis ligula mi. Mauris vitae libero nisi. Nunc vitae felis laoreet, maximus mauris a, congue diam.
+        </p>
+    </div>
+
+    <div class="col-4">
+        <h3 style="text-align: center">Numeros de emergencia</h3>
+        <br/>
+        <img src="https://pbs.twimg.com/media/BtFvNEmCAAEYk30.png" width="100%" height="100%" alt="">
+    </div>
+</div>
