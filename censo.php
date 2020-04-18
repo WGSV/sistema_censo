c30509913e5f66e965e401e19bb950c17475a734
Mario Cabrera <MaritoCx@users.noreply.github.com>
2020-04-17 19:06:58 -0600

    formulario con items

--- censo.php
+++ censo.php
@@ -4,7 +4,9 @@
 </div>
 <form>
   <div class="form-group">
-    <h4>Anfitrión del hogar</h4>
+    <div class="row">
+    <h4>Anfitrión del hogar</h4> <button style="border-radius: 100px; height:20px">i</button>
+    </div>
     <label for="nombres_formulario">Nombres</label>
     <input type="name" class="form-control" id="nombres_formulario" placeholder="Primer y segundo nombre">
     <label for="apellidos_formulario">Apellidos</label>
@@ -25,9 +27,10 @@
           <select class="form-control" id="genero_formulario">
             <option>Masculino</option>
             <option>Femenino</option>
+          </select>
       </div>
       <div class="col-lg-6 col-md-6">
-          <label for="contacto_formulario">Número de contacto</label>
+          <label for="contacto_formulario">Contacto</label>
           <input type="name" class="form-control" id="contacto_formulario" placeholder="0000-0000">
       </div>
     </div>
@@ -39,12 +42,110 @@
           <option>Sonsonate</option>
           <option>Ahuachapán</option>
           <option>Sansalvador</option>
+        </select>
       </div>
       <div class="col-lg-6 col-md-6">
         <label for="municipio_forumulario">Municipio</label>
         <input type="name" class="form-control" id="municipio_formulario" placeholder="Ciudad">
       </div>
     </div>
+    <div class="row">
+      <h4>Datos del hogar</h4> <button style="border-radius: 100px; height:20px">i</button>
+    </div>
+    <div class="row">
+      <div class="col-lg-6 col-md-6">
+        <label for="integrantes_formulario">Cantidad de personas</label>
+        <select class="form-control" id="integrantes_formulario">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+          <option>9</option>
+          <option>10</option>
+        </select>
+      </div>
+      <div class="col-lg-6 col-md-6">
+        <label for="int_formulario">Cantidad de mascotas</label>
+        <select id="int_formulario" class="form-control">
+        <option>ninguna</option>
+        <option>1</option>
+        <option>2-4</option>
+        <option>5-10</option>
+        </select>
+      </div>
+    </div>
+    <label for="disca_formulario">¿Hay personas con discapacidades?</label>
+    <div class="row">
+      <div class="col-lg-6 col-md-6">
+        <label>Cantidad</label>
+        <select id="disca_formulario">
+          <option>ninguno</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>más</option>
+        </select>
+      </div>
+      <div class="col-lg-6 col-md-6">
+        <label>Seleccione las discapacidades</label>
+        <br>
+        <div class="form-check form-check-inline">
+          <input type="checkbox" class="form-check-input">
+          <label class="form-check-label" for="">discapacidad 1</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input type="checkbox" class="form-check-input">
+          <label class="form-check-label" for="">discapacidad 2</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input type="checkbox" class="form-check-input">
+          <label class="form-check-label" for="">discapacidad 3</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input type="checkbox" class="form-check-input">
+          <label class="form-check-label" for="">discapacidad 4</label>
+        </div>
+      </div>
+    </div>
+    <label for="disca_formulario">¿Hay personas con enfermedades?</label>
+    <div class="row">
+      <div class="col-lg-6 col-md-6">
+        <label>Cantidad</label>
+        <select id="disca_formulario">
+          <option>ninguno</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>más</option>
+        </select>
+      </div>
+      <div class="col-lg-6 col-md-6">
+        <label>Seleccione el tipo de enfermedad</label>
+        <br>
+        <div class="form-check form-check-inline">
+          <input type="checkbox" class="form-check-input">
+          <label class="form-check-label" for="">Enfermedad 1</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input type="checkbox" class="form-check-input">
+          <label class="form-check-label" for="">Enfermedad 2</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input type="checkbox" class="form-check-input">
+          <label class="form-check-label" for="">Enfermedad 3</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input type="checkbox" class="form-check-input">
+          <label class="form-check-label" for="">Enfermedad 4</label>
+        </div>
+      </div>
+    </div>
+    <h4>Situación actual</h4> <button style="border-radius: 100px; height:20px">i</button>
+
 
   <div class="form-group">
     <label for="exampleFormControlSelect1">Example select</label>
